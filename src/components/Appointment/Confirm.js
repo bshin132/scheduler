1ba5ec910c5301c53e584956a137504938bf565d
brian shin <bshin132@gmail.com>
2022-03-08 22:07:11 -0800

    forgot to save this file

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "components/Button";
 
+//CONFIRM COMP
 export default function Confirm(props) {
   return (
     <main className="appointment__card appointment__card--confirm">
--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+//SHOW COMP
 export default function Show(props) {
   return (
     <main className="appointment__card appointment__card--show">
